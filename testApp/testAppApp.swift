ecaaceb636be1deb9e2d0d3b4528585fd22b1af3
tanyaburke <tanyaburke@pursuit.org>
2021-02-11 11:58:48 -0500

    Initial Commit

---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Tanya Burke on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
